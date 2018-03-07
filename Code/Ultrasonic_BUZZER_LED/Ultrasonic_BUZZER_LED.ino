c0dd5b8c20f3aafe9602c520fa18e8bc0392478a
MakramChrahil <37145495+MakramChrahil@users.noreply.github.com>
2018-03-07 14:27:25 +0000

    Add files via upload

---
+++ Code/Ultrasonic_BUZZER_LED/Ultrasonic_BUZZER_LED.ino
@@ -0,0 +1,55 @@
+#define trigPin 11
+#define echoPin 10
+#define LED1 2
+#define LED2 3
+#define LED3 4
+#define LED4 5
+#define BUZZER 8
+
+long tempEcho;
+int  distance ;
+
+void setup() {
+  
+Serial.begin(9600) ;
+pinMode(LED1,OUTPUT);
+pinMode(LED2,OUTPUT);
+pinMode(LED3,OUTPUT);
+pinMode(LED4,OUTPUT);
+pinMode(BUZZER,OUTPUT);
+pinMode(trigPin, OUTPUT);
+pinMode(echoPin, INPUT);
+
+digitalWrite(LED1,LOW); digitalWrite(LED2,LOW);  //initialisation des LEDs
+digitalWrite(LED3,LOW); digitalWrite(LED4,LOW);
+
+
+}
+
+void loop() {
+digitalWrite(trigPin,LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin,HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin,LOW) ;
+
+tempEcho = pulseIn(echoPin,HIGH);   // temps d'impulsion
+distance = tempEcho*0.034/2 ;
+Serial.println(distance);
+
+  if ( distance <22) {  digitalWrite(LED1,HIGH);digitalWrite(BUZZER,HIGH);} 
+            else     {  digitalWrite(LED1,LOW);digitalWrite(BUZZER,LOW); }
+  if ( distance <27)    digitalWrite(LED2,HIGH); 
+            else        digitalWrite(LED2,LOW);
+  if ( distance <32)    digitalWrite(LED3,HIGH);
+            else        digitalWrite(LED3,LOW);
+  if ( distance <37 )   digitalWrite(LED4,HIGH); 
+            else        digitalWrite(LED4,LOW);
+       
+delay(100);
+
+
+
+
+
+}
---
+++ Code/Ultrasonic_LCD/Ultrasonic_LCD.ino
@@ -0,0 +1,48 @@
+#include <LiquidCrystal.h>
+
+#define trigPin 10
+#define echoPin 11
+#define BUZZER 8
+
+
+long tempEcho;
+int  distance ;
+const int RS =2, E =3, D4 =4, D5 =5, D6 =6, D7 = 7;   //RS = 12, E = 6, D4 = 5, D5 = 4, D6 = 3, D7 = 2;
+LiquidCrystal lcd(RS, E, D4, D5, D6, D7);
+
+void setup() {
+Serial.begin(9600) ;
+pinMode(BUZZER, OUTPUT);
+pinMode(trigPin, OUTPUT);
+pinMode(echoPin, INPUT);
+
+
+lcd.begin(16, 2);       // initial nombres columns and rows   
+lcd.print("distance: ") ;
+
+}
+
+void loop() {
+digitalWrite(trigPin,LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin,HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin,LOW) ;
+
+
+tempEcho = pulseIn(echoPin,HIGH);       // temps d'impulsion
+distance = tempEcho*0.034/2 ;          //  Etalonnage
+Serial.println(distance);
+
+lcd.home();
+lcd.print("distance en cm : ") ;
+lcd.setCursor(0,1);
+lcd.print(distance);
+delay(100);
+lcd.clear();
+
+
+
+
+
+}
